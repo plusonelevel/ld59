4ce8486c6c1834c2c4d214bc4e2ae4afb6f9346c
Jakub Freywald <jakub.freywald@gmail.com>
2026-04-18 20:31:19 +0200

    make actual satellites children of PathFollow3Ds

--- autoloads/signals.gd
+++ autoloads/signals.gd
@@ -3,5 +3,7 @@ extends Node
 signal camera_reset
 signal camera_centered(node: Node3D)
 
-signal select_planet
-signal select_satellite(idx: int)
+signal planet_selected
+signal satellite_selected(idx: int)
+
+signal orbit_rotated(dir: Enums.RotateDirection)
--- scenes/main.gd
+++ scenes/main.gd
@@ -1,9 +1,6 @@
 extends Node
 
 @onready var planet := $World/Planet
-@onready var sat1 = $World/Planet/Path3D/PathFollow3D/MeshInstance3D/Area3D
-@onready var sat2 = $World/Planet/Path3D2/PathFollow3D/MeshInstance3D/Area3D
-@onready var sat3 = $World/Planet/Path3D3/PathFollow3D/MeshInstance3D/Area3D
 
 var satellites: Array[Node]
 
@@ -11,15 +8,17 @@ var selection: Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	satellites = get_tree().get_nodes_in_group("satellite")
+	satellites = get_tree().get_nodes_in_group("satellite").filter(func(node): return node is Satellite)
 
-	Signals.select_planet.connect(_on_planet_selected)
-	Signals.select_satellite.connect(_on_satellite_selected)
+	Signals.planet_selected.connect(_on_planet_selected)
+	Signals.satellite_selected.connect(_on_satellite_selected)
+	Signals.orbit_rotated.connect(_on_orbit_rotated)
+	
+	Signals.planet_selected.emit()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
 
 func _on_planet_selected() -> void:
 	selection = planet
@@ -31,8 +30,8 @@ func _on_satellite_selected(idx: int) -> void:
 	if sat and sat is Satellite:
 		sat = sat as Satellite
 		selection = sat
-		Signals.camera_centered.emit(sat.get_satellite())
-
-
-func _on_area_3d_area_entered(area: Area3D) -> void:
-	print(area)
+		Signals.camera_centered.emit(sat)
+		
+func _on_orbit_rotated(dir: Enums.RotateDirection) -> void:
+	if selection and selection is Satellite:
+		selection.rotate_orbit(dir)
