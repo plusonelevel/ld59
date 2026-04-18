4f815ddf59fda8739f15e5cf426d44a04972ddf3
Natariin <163893047+Natariin@users.noreply.github.com>
2026-04-18 19:58:45 +0200

    Title screen and theme

--- scenes/ui.gd
+++ scenes/ui.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@onready var mainmenu = $MainMenu
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +10,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_play_pressed() -> void:
+	mainmenu.hide()
