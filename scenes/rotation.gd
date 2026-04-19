977c9c5d2ac9b5d1c6731cd3cf4b23fb30f796f2
Natariin <163893047+Natariin@users.noreply.github.com>
2026-04-19 20:28:11 +0200

    Another planet + positioning the other planets

--- scenes/rotation.gd
+++ scenes/rotation.gd
@@ -3,8 +3,10 @@ extends Node3D
 
 @export var y_speed : float
 @export var z_speed : float
+@export var x_speed : float
 	
 func _physics_process(delta: float) -> void:
 	rotate_y(delta * y_speed)
 	rotate_z(delta * z_speed)
+	rotate_x(delta * x_speed)
 	
