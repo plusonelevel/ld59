199d969cc29ac7c6f312db83340cd511b59d2b29
Jakub Freywald <jakub.freywald@gmail.com>
2026-04-19 10:06:40 +0200

    satellite changes

--- scenes/satellite.gd
+++ scenes/satellite.gd
@@ -1,17 +1,21 @@
 class_name Satellite
-extends Node3D
+extends StaticBody3D
 
 @onready var path = $"../.."
 @onready var path_follow = $".."
 @onready var orbit = $"../../CSGPolygon3D"
 
 @export var speed := 300.0
+var target_rotation
+var elapsed := 0.0
+
+@export var activated := false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	assert(path and path is Path3D and path_follow and path_follow is PathFollow3D, "Satellite must be a child of PathFollow3D!")
-	
+	target_rotation = path.rotation.x
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
@@ -19,7 +23,12 @@ func _physics_process(delta: float) -> void:
 
 
 func rotate_orbit(dir: Enums.RotateDirection) -> void:
-	var diff = 0.1 if dir == Enums.RotateDirection.Right else -0.1
+	var diff := 0.1 if dir == Enums.RotateDirection.Right else -0.1
 	path.rotation.x += diff
 	orbit.rotation.x -= diff
 	orbit.operation = orbit.operation
+
+
+func activate() -> void:
+	activated = true
+	$SatelliteMesh.scale = Vector3(2.0, 2.0, 2.0)
