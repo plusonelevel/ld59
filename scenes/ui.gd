81c0b17238f4cd9a703cbeccc21137bc4b42c07e
Natariin <163893047+Natariin@users.noreply.github.com>
2026-04-19 12:14:09 +0200

    UI buttons, theme and UI

--- scenes/ui.gd
+++ scenes/ui.gd
@@ -4,7 +4,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$MainMenu/Panel/VBoxContainer/Play.pressed.connect(_on_play_pressed)
+	$MainMenu/VBoxContainer/Play.pressed.connect(_on_play_pressed)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
