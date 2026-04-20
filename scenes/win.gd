a60218a531e9ad42a6c37900358ea958c0ee4128
Natariin <163893047+Natariin@users.noreply.github.com>
2026-04-21 00:46:28 +0200

    Win and loss scenes

---
+++ scenes/win.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+@onready var anim = $AnimationPlayer
+
+func _ready() -> void:
+	anim.play("win_anim")
+
+
+func _on_back_to_main_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
