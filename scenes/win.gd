d39f302de2540b4a9d1468ff976464946467458f
Natariin <n.pojer@gmail.com>
2026-04-21 13:56:33 +0200

    Outro buttons fix
    On some occasions the outro button couldnt be pressed, this is a workaround

--- scenes/win.gd
+++ scenes/win.gd
@@ -5,7 +5,9 @@ extends Node3D
 func _ready() -> void:
 	anim.play("win_anim")
 	Engine.time_scale = 1
+	
+	anim.animation_finished.connect(_on_anim_finished)
 
-
-func _on_back_to_main_pressed() -> void:
+func _on_anim_finished(anim_name: String):
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
+		
